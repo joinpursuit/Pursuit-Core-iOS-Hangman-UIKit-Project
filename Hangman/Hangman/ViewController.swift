5378914cde524103b41aaea4968cc1b308aa310e
janezhu1618 <janezhu@pursuit.org>
2018-11-20 17:01:13 -0500

    2nd commit - in process of setting up the viewcontroller

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Hangman
 //
-//  Created by Alex Paul on 11/19/18.
+//  Created by Jane Zhu on 11/19/18.
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
@@ -13,22 +13,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var wordTF: UITextField!
     @IBOutlet weak var guessTF: UITextField!
     @IBOutlet weak var wordDisplay: UILabel!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var hangImage: UIImageView!
+    
+    private var theWord: String = ""
     
     override func viewDidLoad() {
     super.viewDidLoad()
     wordTF.delegate = self
     guessTF.delegate = self
   }
+
+    private func displayLines(_ userWord: String) {
+        var displayStr: String = ""
+        for _ in displayStr {
+            displayStr += "_ "
+        }
+        message.text = displayStr
+    }
+    
+    
 }
+
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print(textField.text ?? "")
+    // https://grokswift.com/uitextfield/ this source helped me with this section
+        let characterSetAllowed = CharacterSet.letters
+        if textField == wordTF {
+            if let _ = string.rangeOfCharacter(from: characterSetAllowed, options: .caseInsensitive) {
+                theWord = textField.text!
+                return true
+            } else {
+                return false
+            }
+    } else if textField == guessTF {
+        let characterCountLimit = 1
+        let startingLength = textField.text?.count ?? 0
+        let lengthToAdd = string.count
+        let lengthToReplace = range.length
+        let newLength = startingLength + lengthToAdd - lengthToReplace
+        return newLength <= characterCountLimit
+            
+    }
         return true
     }
-    
 }
