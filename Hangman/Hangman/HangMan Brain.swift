65fc30b050f910c0ccc334f56d9c2d21348cae1f
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-11-24 10:58:15 -0500

    setUp

---
+++ Hangman/Hangman/HangMan Brain.swift
@@ -0,0 +1,26 @@
+//
+//  HangMan Brain.swift
+//  Hangman
+//
+//  Created by Elizabeth Peraza  on 11/23/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class HangmanBrain {
+
+  var instructions = """
+  Player2: Enter letters below to guess the word.
+  Letters to choose from:
+  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+  """
+
+  var hiddenWord = UITextField()
+  var letterByUser = UITextField()
+  
+  func beginPlay(){
+
+  }
+  
+}
--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -9,14 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var hangManBrain = HangmanBrain()
+  
 
+  @IBOutlet weak var hangmanTitle: UILabel!
+  
+  @IBOutlet weak var hiddenWord: UITextField!
+  
+  @IBOutlet weak var hangImage: UIImageView!
+  
+  @IBOutlet weak var instructions: UITextView!
+  
+  @IBOutlet weak var letterByUser: UITextField!
+  
+  @IBOutlet weak var displayRightChoices: UILabel!
+  
+  @IBOutlet weak var newGame: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    print("TEST TEST TEST TEST")
-
+    instructions.text = hangManBrain.instructions
+    letterByUser.delegate = self
+    hiddenWord.delegate = self
   }
+}
+
+extension ViewController: UITextFieldDelegate {
+  internal func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+    textField.resignFirstResponder()
+    if textField != nil {
+      hangManBrain.hiddenWord = textField
+      print("This is the hiddenWord: \(hangManBrain.hiddenWord.text!)")
+    } else if textField != nil {
+     hangManBrain.letterByUser = textField
+      print("This is the hiddenWord: \(hangManBrain.letterByUser.text!)")
 
 
+    }
+    return true
+    
+    
+    
+    
+  }
+  
+  internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool{
+//    print(textField.text ?? "")
+    return true
+  }
+  
+//  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//    let currentText = textField.text ?? ""
+//    guard let stringRange = Range(range, in: currentText) else {return false}
+//    let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+//    return updatedText.count <= 1
+//  }
+  
+  
 }
 
