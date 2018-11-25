c79939d06dbce64c372524de199799dd804fe0ba
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-11-25 10:21:15 -0500

    backUp, not a working product yet

--- Hangman/Hangman/HangMan Brain.swift
+++ Hangman/Hangman/HangMan Brain.swift
@@ -8,19 +8,121 @@
 
 import UIKit
 
-class HangmanBrain {
 
+enum Image: String {
+  case hang1 = "hang1"
+  case hang2 = "hang2"
+  case hang3 = "hang3"
+  case hang4 = "hang4"
+  case hang5 = "hang5"
+  case hang6 = "hang6"
+  case hang7 = "hang7"
+  
+  func setImage(hangPic: String) -> String {
+    let imageTitle = self.rawValue
+    
+    return imageTitle
+  }
+  
+}
+
+
+class HangmanBrain {
+  
+  var hiddenWordLenght = 20
+  var letterByUserLength = 1
+  
   var instructions = """
   Player2: Enter letters below to guess the word.
   Letters to choose from:
   a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
   """
-
-  var hiddenWord = UITextField()
-  var letterByUser = UITextField()
   
-  func beginPlay(){
+  
+  
+  var hiddenWord = UITextField()
+  var hiddenWordArray = [Character]()
+  var letterByUserTwo: Character = " "
+  var letterByUser = UITextField()
+  var blanks = " "
+  var blankArray = [Character]()
+  var counterFailedAttempts = 0
+  var counterForGuesses = 0
+ var indexForLetter = 0
+  var hangmanImage = UIImage(named: " ")
 
+  
+  func unwrappingResponse() {
+    let userEnteredLetter = letterByUser.text?.lowercased() ?? " "
+    switch userEnteredLetter.lowercased() {
+    case "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z":
+      counterForGuesses += 1
+    default:
+      print("Not a valid entry. Please try again.")
+    }
   }
   
+  func settingUpBlanks(word: UITextField){
+    blanks = String(repeating: "_", count: word.text!.count)
+    blankArray = Array(blanks)
+  }
+  
+  func handleHiddenWord(wordToBeGuessed: UITextField) -> [Character] {
+    let arrayWordToPlay: [Character] = Array(wordToBeGuessed.text ?? " ")
+    return arrayWordToPlay
+  }
+  
+  func handleLetterByUser2 (letter:UITextField) -> Character {
+    let letterToCharacter = Character(letter.text ?? "!")
+    return letterToCharacter
+  }
+    
+  func beginPlay(hiddenWordArray: [Character], letterByUserTwo: Character) {
+
+    if hiddenWordArray.contains(letterByUserTwo){
+      print("hidden word contains letter")
+      for (index, element) in hiddenWordArray.enumerated(){
+        if letterByUserTwo == element{
+          indexForLetter = index
+          blankArray[indexForLetter] = element
+          
+        }
+      }
+    } else if !hiddenWordArray.contains(letterByUserTwo){
+      counterFailedAttempts += 1
+      switch counterFailedAttempts{
+      case 5:
+        hangmanImage = UIImage(named: "hang5")!
+        
+        print("")
+        
+      case 4:
+        
+        hangmanImage = UIImage(named: "hang4")!
+        print("")
+        
+      case 3:
+        hangmanImage = UIImage(named: "hang3")!
+        print("")
+        
+      case 2:
+        hangmanImage = UIImage(named: "hang2")!
+        print("")
+        
+      case 1:
+        hangmanImage = UIImage(named: "hang1")!
+        print("")
+        
+      case 6:
+        hangmanImage = UIImage(named: "hang6")!
+        print ("The rigth word was: \(hiddenWord.text!.uppercased())")
+        print("")
+        
+        counterFailedAttempts = 7
+        
+      default:
+        print("You are doneso... 👻")
+      }
+    }
+  }
 }
--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -12,20 +12,15 @@ class ViewController: UIViewController {
   
   var hangManBrain = HangmanBrain()
   
-
+  
   @IBOutlet weak var hangmanTitle: UILabel!
-  
   @IBOutlet weak var hiddenWord: UITextField!
-  
   @IBOutlet weak var hangImage: UIImageView!
-  
   @IBOutlet weak var instructions: UITextView!
-  
   @IBOutlet weak var letterByUser: UITextField!
-  
   @IBOutlet weak var displayRightChoices: UILabel!
-  
   @IBOutlet weak var newGame: UITextField!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -33,39 +28,108 @@ class ViewController: UIViewController {
     letterByUser.delegate = self
     hiddenWord.delegate = self
   }
+  
+  
+  @IBAction func beginPlay() {
+    hangManBrain.beginPlay(hiddenWordArray: hangManBrain.hiddenWordArray, letterByUserTwo: hangManBrain.letterByUserTwo)
+    displayRightChoices.text = "\(hangManBrain.blanks)"
+    print(hangManBrain.blanks)
+    
+  }
+  
+  
+  
 }
+
 
 extension ViewController: UITextFieldDelegate {
-  internal func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+  
+  //textFieldShouldReturn is called when the keyboard return key is pressed. This method is used to dismiss the keyboard
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     textField.resignFirstResponder()
-    if textField != nil {
+    
+    if textField == hiddenWord{
       hangManBrain.hiddenWord = textField
+      hangManBrain.hiddenWordArray = hangManBrain.handleHiddenWord(wordToBeGuessed: hiddenWord)
+      
       print("This is the hiddenWord: \(hangManBrain.hiddenWord.text!)")
-    } else if textField != nil {
-     hangManBrain.letterByUser = textField
-      print("This is the hiddenWord: \(hangManBrain.letterByUser.text!)")
-
-
+    } else if textField == letterByUser{
+      hangManBrain.letterByUser = textField
+      hangManBrain.letterByUserTwo = hangManBrain.handleLetterByUser2(letter: letterByUser)
+      print("This is the letter: \(hangManBrain.letterByUser.text!)")
     }
-    return true
     
-    
-    
-    
-  }
-  
-  internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool{
-//    print(textField.text ?? "")
     return true
   }
   
-//  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//    let currentText = textField.text ?? ""
-//    guard let stringRange = Range(range, in: currentText) else {return false}
-//    let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-//    return updatedText.count <= 1
-//  }
+  //shouldChange Character is called when the user types a new character in the text field or deletes an existing character. This method will also disallow the use of a certain character
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    if textField == hiddenWord {
+      let currentLetter = letterByUser.text ?? ""
+      guard let otherStringRange = Range(range, in: currentLetter) else {return false}
+      let updatedLetterByUser = currentLetter.replacingCharacters(in: otherStringRange, with: string)
+      return updatedLetterByUser.count <= hangManBrain.letterByUserLength
+    } else if textField == letterByUser {
+      let currentLetter = letterByUser.text ?? ""
+      guard let otherStringRange = Range(range, in: currentLetter) else {return false}
+      let updatedLetterByUser = currentLetter.replacingCharacters(in: otherStringRange, with: string)
+      return updatedLetterByUser.count <= hangManBrain.letterByUserLength
+    }
+    
+    return true
+  }
   
+  //is called when the clearButton is pressed
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    print("clear button pressed")
+    return true
+  }
+  
+  
+  
+  //  internal func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+  //    textField.resignFirstResponder()
+  //    if textField == hiddenWord {
+  //      hangManBrain.hiddenWord = textField
+  //      print("This is the hiddenWord: \(hangManBrain.hiddenWord.text!)")
+  //    } else if textField == letterByUser {
+  //      hangManBrain.letterByUser = textField
+  //      print("This is the letter: \(hangManBrain.letterByUser.text!)")
+  //
+  //    }
+  //    return true
+  //
+  //  }
+  //
+  //  internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool{
+  //
+  //    let currentLetter = letterByUser.text ?? ""
+  //    guard let otherStringRange = Range(range, in: currentLetter) else {return false}
+  //    let updatedLetterByUser = currentLetter.replacingCharacters(in: otherStringRange, with: string)
+  //    return updatedLetterByUser.count <= hangManBrain.letterByUserLength
+  //  }
   
 }
+
+////called when editing is about to begin
+//func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//  print("editing is about to begin")
+//  return true
+//}
+////is called when editing has begun
+//func textFieldDidBeginEditing(_ textField: UITextField) {
+//  textField.backgroundColor = UIColor.gray
+//  print("editing bagun")
+//}
+////is called when the editing is about to end
+//func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//  print("editing about to end")
+//  return true
+//}
+////is called when editing ended
+//func textFieldDidEndEditing(_ textField: UITextField) {
+//  print("editing ended")
+//  textField.backgroundColor = UIColor.white
+//}
 
