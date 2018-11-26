cba1828e1ea662053e675ea7ea11a08212b743ec
BironSu <bironsu@pursuit.org>
2018-11-25 21:37:32 -0500

    Finished the project, just got to make it nicer and condense it if possible.

--- Hangman/Hangman/Controller/ViewController.swift
+++ Hangman/Hangman/Controller/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     var hangmanCalc = Hangman()
+    var gameWon = false
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var middleLabel: UILabel!
-
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var newGame: UIButton!
+    @IBOutlet weak var badPile: UILabel!
+    @IBOutlet weak var totalTries: UILabel!
+    @IBOutlet weak var imageMan: UIImageView!
     override func viewDidLoad() {
     super.viewDidLoad()
         topTextField.delegate = self
@@ -23,10 +26,8 @@ class ViewController: UIViewController {
         bottomTextField.isEnabled = false
   }
 }
-
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         if (textField == topTextField) {
             hangmanCalc.createArrays(a: topTextField.text?.lowercased() ?? "Not A String")
             topTextField.isEnabled = false
@@ -37,28 +38,36 @@ extension ViewController: UITextFieldDelegate {
             return true
         } else if (textField == bottomTextField) {
             hangmanCalc.guessWord(b: bottomTextField.text?.lowercased() ?? "Not A String")
+            middleLabel.text = hangmanCalc.middleLabel
+            badPile.text = hangmanCalc.badLabel
             bottomTextField.text = ""
             bottomTextField.resignFirstResponder()
+            totalTries.text = "Tries: \(hangmanCalc.tries)"
+            gameWon = hangmanCalc.gameDone
+            imageMan.image = hangmanCalc.imageMan
+            if gameWon {
+                bottomTextField.isEnabled = false
+            }
             return true
         }
         return true
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let characters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyz")
-        if (string.lowercased().rangeOfCharacter(from: characters) == nil) {
-            middleLabel.text = "Enter characters only!"
-            return false
-        } else {
+        if (string.rangeOfCharacter(from: characters) != nil) || (string == "" && range.length > 0) {
             middleLabel.text = hangmanCalc.middleLabel
+            return bottomTextField.text!.count < 1 || string == ""
+        } else {
+            middleLabel.text = "Enter alphabets only!"
+            return false
         }
-        return bottomTextField.text!.count < 1 || string == ""
     }
     @IBAction func newGame (_ sender: UIButton) {
+        hangmanCalc.reset()
         topTextField.isEnabled = true
         topTextField.text = ""
-        middleLabel.text = "HangMan!"
+        middleLabel.text = hangmanCalc.middleLabel
         bottomTextField.isEnabled = false
         bottomTextField.text = ""
-        hangmanCalc.reset()
     }
 }
--- Hangman/Hangman/Model/Model.swift
+++ Hangman/Hangman/Model/Model.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Hangman {
     var answerArray = [String]()
@@ -14,8 +14,14 @@ class Hangman {
     var badArray = [String]()
     var topTextField: String = ""
     var middleLabel: String = ""
-    var bottomTextField:Bool = false
+    var badLabel: String = ""
+    var tries: Int = 0
+    var badTries: Int = 0
+    var bottomTextField: Bool = false
+    var gameDone: Bool = false
+    var imageMan: UIImage = UIImage(named: "hang1")!
     func createArrays(a: String) {
+        gameDone = false
         for i in a {
             answerArray.append(String(i))
             guessArray.append("_")
@@ -26,25 +32,48 @@ class Hangman {
     func guessWord (b: String) {
         if answerArray.contains(b) {
             if guessArray.contains(b) {
-                middleLabel = "You've already picked this letter"
-                sleep(2)
-                middleLabel = guessArray.compactMap{$0}.joined(separator: " ")
-            }
-            for i in 0..<answerArray.count {
-                for _ in answerArray {
-                    if answerArray[i] == b {
-                        guessArray[i] = answerArray[i]
-                        if guessArray == answerArray {
-                            middleLabel = "Winner! The word is \(guessArray.compactMap{$0}.joined(separator: " "))"
+                middleLabel = "You've already entered \(b)."
+            } else {
+                tries += 1
+                for i in 0..<answerArray.count {
+                    for _ in answerArray {
+                        if answerArray[i] == b {
+                            guessArray[i] = answerArray[i]
+                            if guessArray == answerArray {
+                                middleLabel = "Winner! The word is \(guessArray.compactMap{$0}.joined())!"
+                                gameDone = true
+                                break
+                            }
+                            middleLabel = guessArray.compactMap{$0}.joined(separator: " ")
                         }
                     }
                 }
             }
-            
+        } else {
+            if badArray.contains(b) {
+                middleLabel = "You've already entered the letter \(b)."
+            } else {
+                tries += 1
+                badTries += 1
+                badArray.append(b)
+                middleLabel = "Sorry, word doesn't contain the letter \(b)."
+                badLabel = "Bad pile : \(badArray.compactMap{$0}.joined(separator: ", "))"
+            }
         }
+        if badTries > 1 && badTries < 7 {
+            imageMan = UIImage(named: "hang\(badTries)")!
+        } else if badTries == 7 {
+            imageMan = UIImage(named: "hang\(badTries)")!
+            middleLabel = "GAME OVER! The dude died."
+            gameDone = true
+        }
+        
     }
     func reset() {
         answerArray = []
         guessArray = []
+        middleLabel = "Hangman!"
+        tries = 0
+        badTries = 0
     }
 }
