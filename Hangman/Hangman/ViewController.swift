75c984d829508df85797edbee2929ab8c38ce929
janezhu1618 <janezhu@pursuit.org>
2018-11-27 10:27:35 -0500

    11th commit - implemented one and two player modes on the same viewcontroller

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -8,11 +8,9 @@
 
 import UIKit
 
-// to-do
-// need to fix issue with typing in repeating letters
-// need to fix issue with tracking for win - repeated letters don't count
-
 class ViewController: UIViewController {
+    
+    var instance = GameModel()
 
     @IBOutlet weak var wordTF: UITextField!
     @IBOutlet weak var guessTF: UITextField!
@@ -22,6 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var strikeNumDisplay: UILabel!
     @IBOutlet weak var rightGuess: UILabel!
     @IBOutlet weak var wrongGuess: UILabel!
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     wordTF.delegate = self
@@ -29,78 +28,79 @@ class ViewController: UIViewController {
     guessTF.isEnabled = false
   }
     
-    
-    func checkForGameOver() {
-        if Brain.gameOver {
-            message.text = "YOU WIN."
-            guessTF.isEnabled = false
-            strikeNumDisplay.text = "Press \"New Game\" to begin"
-        }
-        if Brain.strikeCounter == 7 {
-            message.text = "GAME OVER."
-            guessTF.isEnabled = false
-            strikeNumDisplay.text = "The word is \(Brain.theWord)"
-        }
-    }
-    
-    @IBAction func reset(_ sender: UIButton) {
-        Brain.reset()
-        wordTF.text = ""
-        wordTF.isEnabled = true
+    @IBAction func newGame(_ sender: UIButton) {
         guessTF.text = ""
-        guessTF.isEnabled = false
-        strikeImageDisplay.isHidden = true
-        strikeNumDisplay.isHidden = true
-        wordDisplay.text = "waiting for word input"
-        message.text = "Enter a word."
+        strikeImageDisplay.image = instance.strikeImageUpdater()
+        strikeNumDisplay.text = ""
         rightGuess.text = "Right Guess:"
         wrongGuess.text = "Wrong Guess:"
+        if sender.tag == 0 {
+            GameModel.onePlayerMode = true
+            instance.reset()
+//            instance.word = instance.getWord()
+            wordTF.isHidden = true
+            wordTF.isEnabled = false
+            guessTF.isEnabled = true
+            wordDisplay.text = instance.displayUnderscores()
+            message.text = "Enter a guess letter."
+        }
+        if sender.tag == 1 {
+            GameModel.onePlayerMode = false
+            instance.reset()
+            wordTF.text = ""
+            wordTF.isHidden = false
+            wordTF.isEnabled = true
+            guessTF.isEnabled = false
+            wordDisplay.text = "waiting for word input..."
+            message.text = "Enter a word."
+        }
+        
+        
     }
+
+func checkForGameOver() {
+    if instance.win {
+        message.text = "YOU WIN 😃😃😃"
+        guessTF.isEnabled = false
+        strikeNumDisplay.text = "Choose a game mode to restart."
+    }
+    if instance.strikeCounter == 7 {
+        message.text = "GAME OVER 😨😨😨"
+        guessTF.isEnabled = false
+        strikeNumDisplay.text = "The word is \"\(instance.word)\""
+    }
+}
 }
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text?.isEmpty ?? true {
+            return true
+        }
         textField.resignFirstResponder()
         if textField == wordTF {
-            if textField.text?.isEmpty ?? true {
-                return true
-            }
-            Brain.theWord = textField.text!.lowercased()
-            wordDisplay.text = Brain.displayUnderscores()
+            instance.word = textField.text!.lowercased()
+            wordDisplay.text = instance.displayUnderscores()
             message.text = "Enter a guess letter."
             guessTF.isEnabled = true
-            strikeImageDisplay.isHidden = false
-            strikeNumDisplay.isHidden = false
             wordTF.isEnabled = false
             return true
             }
     
         if textField == guessTF {
-            if textField.text?.isEmpty ?? true {
-                return true
-            }
-        Brain.guessLetter = textField.text!.lowercased()
-        message.text = Brain.isLetterInWord()
-        rightGuess.text = "Right Guess: \(Brain.rightGuess.compactMap{String($0)}.joined(separator: ", "))"
-        wrongGuess.text = "Wrong Guess: \(Brain.wrongGuess.compactMap{String($0)}.joined(separator: ", "))"
-        wordDisplay.text = String(Brain.displayWord.compactMap{String($0)}.joined(separator: " "))
-        strikeImageDisplay.image = Brain.strikeImageUpdater()
-        strikeNumDisplay.text = "Strike " + Brain.strikeCounter.description + "/7"
+        instance.currentGuess = textField.text!.lowercased()
+        message.text = instance.isLetterInWord()
+        rightGuess.text = "Right Guess: \(instance.rightGuessArr.compactMap{String($0)}.joined(separator: ", "))"
+        wrongGuess.text = "Wrong Guess: \(instance.wrongGuessArr.compactMap{String($0)}.joined(separator: ", "))"
+        wordDisplay.text = String(instance.displayWordAsArr.compactMap{String($0)}.joined(separator: " "))
+        strikeImageDisplay.image = instance.strikeImageUpdater()
+        strikeNumDisplay.text = "Strike " + instance.strikeCounter.description + "/7"
         checkForGameOver()
-    }
-        return true
-    }
-    
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        if textField == guessTF {
-        return true
         }
         return true
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-    // https://grokswift.com/uitextfield/ this source helped me with this section
         if string.rangeOfCharacter(from: CharacterSet.letters, options: .caseInsensitive) != nil || (string == "" && range.length > 0) {
             return guessTF.text!.count < 1
         } else {
@@ -109,3 +109,4 @@ extension ViewController: UITextFieldDelegate {
     }
 }
 
+   // https://grokswift.com/uitextfield/ this source helped me with the UITextField section
