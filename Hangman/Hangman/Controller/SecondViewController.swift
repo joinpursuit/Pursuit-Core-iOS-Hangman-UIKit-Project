910fd03eb2befac01faf515027284d1e6db17e0a
BironSu <bironsu@pursuit.org>
2018-11-27 13:46:56 -0500

    Attempting single player, pushing incase I break my code

---
+++ Hangman/Hangman/Controller/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Hangman
+//
+//  Created by Biron Su on 11/27/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
