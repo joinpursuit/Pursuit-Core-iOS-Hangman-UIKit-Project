e37eec195ca9aeabedb89d0e80dd016f3cde354d
Casandra <casandragrullon@pursuit.org>
2019-11-14 10:07:13 -0500

    adding properties and methods to game model.

--- Hangman/Hangman/GameModel.swift
+++ Hangman/Hangman/GameModel.swift
@@ -14,7 +14,26 @@ enum GameResult {
 }
 
 class GameModel {
-    var winningWord = ""
+    var numOfGuessesWrong : Int
+    var count : Int
+    var indicies : Set<Int>
+    var player1WordInput : String
+    
+    init(numOfGuessesWrong: Int, count: Int, indices: Set<Int>, player1WordInput: String){
+        self.numOfGuessesWrong = numOfGuessesWrong
+        self.count = count
+        self.indicies = indices
+        self.player1WordInput = player1WordInput
+    }
+    
+    static func getDashes(word: String) -> [String] {
+        let dashes = Array(repeating: "_", count: word.count)
+        return dashes
+    }
+    
+    static func winCondition() {
+        
+    }
     
     
 }
--- Hangman/Hangman/Player1ViewController.swift
+++ Hangman/Hangman/Player1ViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class Player1ViewController: UIViewController {
 
+    @IBOutlet weak var player1Text: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player1Text.delegate = self
+        
     }
     
+    @IBAction func player1TextAction(_ sender: Any) {
+    }
+    
+    @IBAction func startGameButton(_ sender: UIButton) {
+    }
+    
+}
 
-
+extension Player1ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        for char in string {
+            
+        }
+        return true
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
 }
