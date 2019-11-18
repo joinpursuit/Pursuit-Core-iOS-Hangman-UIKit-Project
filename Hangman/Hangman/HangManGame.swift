48b6de305623deb2ef6c73f80ad35ef975f71a79
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-11-18 13:26:33 -0500

    Completed Base Credit

--- Hangman/Hangman/HangManGame.swift
+++ Hangman/Hangman/HangManGame.swift
@@ -17,13 +17,12 @@ enum GuessResult: CaseIterable    {
 
 struct HangManGame  {
     var hiddenWord: String
-    
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
                     "n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    
     var usedLetters: Set<String>? = [] // optional if no letters gussed
-    
     var chances = 6
+    var p1Wins = false
+    var p2Wins = false
     
     func hideWord() -> String {
         var wordToHide = ""
@@ -33,17 +32,39 @@ struct HangManGame  {
         return wordToHide
     }
     
+    func revealLetter(letterGuess: String, hiddenWord: String) -> String {
+        var wordtToReveal = hiddenWord
+        var indexL = 0
+        var revealedLetter = String()
+        print(wordtToReveal)
+        for (index, char) in self.hiddenWord.enumerated()  {
+            if String(char) == letterGuess  {
+                let startIndex = wordtToReveal.startIndex
+                let offset = wordtToReveal.index(startIndex, offsetBy: indexL)
+                wordtToReveal.remove(at: offset)
+                wordtToReveal.insert(Character(letterGuess), at: offset)
+                revealedLetter = wordtToReveal
+                indexL += 2
+            } else  {indexL += 2}
+        }
+        indexL = 0
+        return revealedLetter
+    }
+    
     func gameGuess(letterGuess: String) -> GuessResult  {
         let guessResult:GuessResult
-        if hiddenWord.contains(letterGuess)   {
-            guessResult = .correctGuess
-        }
-        else if usedLetters?.contains(letterGuess) ?? false   {
+        
+        if usedLetters?.contains(letterGuess) ?? false   {
             guessResult = .redundantGuess
         }
+        else if hiddenWord.contains(letterGuess)  {
+            guessResult = .correctGuess
+        }
+            
         else    {
             guessResult = .IncorrectGuess
         }
         return guessResult
     }
+    
 }
--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -14,72 +14,151 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var hangmanImage: UIImageView!
     @IBOutlet weak var player1TextField: UITextField!
-    @IBOutlet weak var player2Textfield: UITextField!
+    @IBOutlet weak var player2TextField: UITextField!
     @IBOutlet weak var secretWordLabel: UILabel!
     @IBOutlet weak var guessLetterLabel: UILabel!
     @IBOutlet weak var winnerLabel:  UILabel!
     @IBOutlet weak var hiddenWordLabel: UILabel!
     
     
+    
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
         
-        //hangmanImage.image = #imageLiteral(resourceName: "hang1")
-        player2Textfield.isEnabled = false
-        player2Textfield.isHidden = true
+        player2TextField.isEnabled = false
+        player2TextField.isHidden = true
         guessLetterLabel.isHidden = true
+        secretWordLabel.isHidden = false
         player1TextField.delegate = self
-        player2Textfield.delegate = self
-  }
-
+        player2TextField.delegate = self
+    }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        winnerLabel.text = "Winner:"
+        hangmanImage.image = #imageLiteral(resourceName: "hang1")
+        hangManGame.chances = 6
+        player2TextField.resignFirstResponder()
+        player2TextField.isEnabled = false
+        player2TextField.isHidden = true
+        guessLetterLabel.isHidden = true
+        player1TextField.isEnabled = true
+        player1TextField.isHidden = false
+        player1TextField.text = ""
+        hangManGame.hiddenWord = ""
+        hiddenWordLabel.text = ""
+        player2TextField.text = ""
+        hangManGame.usedLetters = []
+        hangManGame.p1Wins = false
+        hangManGame.p2Wins = false
+        secretWordLabel.isHidden = false
+    }
+    
+    
 }
 
 extension ViewController: UITextFieldDelegate   {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if textField == player2Textfield    {
+        if textField == player1TextField    {
+            
+            if hangManGame.alphabet.contains(string) == false || string == "" {return false}
             
             guard let text = textField.text else {
                 return false
             }
-            let currentText = text + string
-            var guessResult = hangManGame.gameGuess(letterGuess: string)
-            switch guessResult {
-            case .correctGuess:
-                print("Correct")
-            case .IncorrectGuess:
-                print("Incorrect")
-            case .redundantGuess:
-                print("redundant")
-            }
-            if currentText.count > 1    {
-                textField.text = string
+            
+        }
+        if textField == player2TextField    {
+            
+            if hangManGame.alphabet.contains(string) == false || string == "" {return false}
+            
+            guard let text = textField.text else {
                 return false
             }
             
+            let currentText = text + string
+            let guessResult = hangManGame.gameGuess(letterGuess: string)
             
+            switch guessResult {
+            case .correctGuess:
+                hiddenWordLabel.text = hangManGame.revealLetter(letterGuess: string, hiddenWord: hiddenWordLabel.text ?? "default")
+                print(hiddenWordLabel.text ?? "default")
+                let wordToCheck = hiddenWordLabel.text?.replacingOccurrences(of: " ", with: "") ?? "default"
+                if wordToCheck == hangManGame.hiddenWord    {
+                    hangManGame.p1Wins = true
+                }
+                print("Correct")
+                print(wordToCheck)
+            case .IncorrectGuess:
+                hangManGame.chances -= 1
+                switch hangManGame.chances  {
+                case 5:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang2")
+                case 4:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang3")
+                case 3:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang4")
+                case 2:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang5")
+                case 1:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang6")
+                case 0:
+                    hangmanImage.image = #imageLiteral(resourceName: "hang7")
+                    hangManGame.p2Wins = true
+                default:
+                    print("default")
+                }
+                //print(hangManGame.chances)
+                print("Incorrect")
+            case .redundantGuess:
+                print("redundant")
+                return false
+            }
             
+            if hangManGame.p1Wins == true   {
+                winnerLabel.text = "Winner: Player 1!"
+                player2TextField.resignFirstResponder()
+                player2TextField.text = ""
+                player2TextField.isEnabled = false
+                player2TextField.isHidden = true
+                guessLetterLabel.isHidden = true
+            }
+                
+            else if hangManGame.p2Wins == true  {
+                winnerLabel.text = "Winner: Player 2!" + " \(hangManGame.hiddenWord)"
+                player2TextField.resignFirstResponder()
+                player2TextField.text = ""
+                player2TextField.isEnabled = false
+                player2TextField.isHidden = true
+                guessLetterLabel.isHidden = true
+            }
+            
+            hangManGame.usedLetters?.insert(string)
+            print(hangManGame.usedLetters ?? "default")
+            if currentText.count > 1   {
+                textField.text = string
+                return false
+            }
         }
+        
         return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        player1TextField.resignFirstResponder()
-        player1TextField.isEnabled = false
-        player1TextField.isHidden = true
-        //player 1's word
-        hangManGame.hiddenWord = player1TextField.text ?? "default"
-        let hiddenWord = hangManGame.hideWord()
-        secretWordLabel.isHidden = true
-        hiddenWordLabel.text = hiddenWord
-    
-        
-        player2Textfield.isHidden = false
-        player2Textfield.isEnabled = true
-        guessLetterLabel.isHidden = false
-        
+        if textField == player1TextField {
+            player1TextField.resignFirstResponder()
+            player1TextField.isEnabled = false
+            player1TextField.isHidden = true
+            //player 1's word
+            hangManGame.hiddenWord = player1TextField.text ?? "default"
+            let hiddenWord = hangManGame.hideWord()
+            secretWordLabel.isHidden = true
+            hiddenWordLabel.text = hiddenWord
+            
+            player2TextField.isHidden = false
+            player2TextField.isEnabled = true
+            guessLetterLabel.isHidden = false
+        }
         
         return true
     }
